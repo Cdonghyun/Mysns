af5d8fb6765d8c6f7baaac26498875d6ff789a53
eojin <chasg8840@gmail.com>
2022-04-05 13:23:07 +0900

    Initial commit

---
+++ src/test/java/com/example/Mysns/MysnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mysns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
